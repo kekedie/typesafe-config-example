8277f3c7eca1088c84f05e2c36636e83fc721b52
Han Sangwon <han_sangwon@cyberagent.co.jp>
2014-12-18 13:48:51 +0900

    rename class name

--- src/main/scala/EnvironmentConfigMain.scala
+++ src/main/scala/EnvironmentConfigMain.scala
@@ -3,12 +3,22 @@ import java.net.URL
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.StrictLogging
 
-object EnvironmentConfig extends StrictLogging {
+object EnvironmentConfigMain extends StrictLogging {
 
+  /**
+   * {{
+   * sbt run
+   * sbt 'run development'
+   * sbt 'run staging'
+   * sbt 'run production'
+   * }}
+   * @param args environment value[development|staging|production]
+   */
   def main(args: Array[String]): Unit = {
     URL.setURLStreamHandlerFactory(new EnvironmentURLStreamHandlerFactory)
     System.setProperty("environment", args.headOption.getOrElse("development"))
     val config = ConfigFactory.load()
     logger.info(config.getString("app.env.message"))
   }
+
 }
